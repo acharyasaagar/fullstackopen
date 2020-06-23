f64a4f74223615ba7a11c637ad290262349d626f
acharyasagar <acharyasaagar@gmail.com>
2020-06-23 17:09:56 +0300

    Exercise 1.2 > Courseinfo step 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Part = (props) => (
+  <>
+    <p>{props.content.part} {props.content.exercises}</p>
+  </>
+)
+
 const Header = (props) => (
   <>
     <h1>{props.course}</h1>
@@ -9,7 +15,11 @@ const Header = (props) => (
 
 const Content = (props) => (
   <>
-    <p>{props.part} {props.exercises}</p>
+    {
+      props.contents.map(item => {
+        return <Part content={item} key={item.part}/>
+      })
+    }
   </>
 )
 
@@ -21,20 +31,27 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const contents = [
+    {
+      part: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      part: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      part: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const total = contents.reduce((acc, cur) => acc + cur.exercises, 0)
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-     <Total total={exercises1 + exercises2 + exercises3} />
+      <Content contents={contents} />
+     <Total total={total} />
     </div>
   )
 }
