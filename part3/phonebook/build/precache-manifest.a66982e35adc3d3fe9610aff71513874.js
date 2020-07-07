f27bfe30c54ccdb9eac3c2af8c5d4c9965e6679b
acharyasagar <acharyasaagar@gmail.com>
2020-07-07 17:59:31 +0300

    uibuild

---
+++ part3/phonebook/build/precache-manifest.a66982e35adc3d3fe9610aff71513874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b5fea331416477c0ab0bdc35087d5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b3455b15491fe51303d",
+    "url": "/static/css/main.1c9681e8.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3455b15491fe51303d",
+    "url": "/static/js/main.94b6d3d1.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
