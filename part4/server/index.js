5ad44efe21d9d9c278decdce5eda17ce42fa80ec
acharyasagar <acharyasaagar@gmail.com>
2020-07-13 16:52:22 +0300

    move blog router to its own module inside controllers dir

--- part4/server/index.js
+++ part4/server/index.js
@@ -2,17 +2,10 @@ const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blog')
 
 const { PORT, MONGODB_URI } = require('./utils/constants')
 const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,19 +15,7 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
