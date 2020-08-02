58bff2f8e245656cf81cb041f08a11d0250869bb
acharyasagar <acharyasaagar@gmail.com>
2020-08-02 09:10:36 +0300

    Create a patch route to like the blog, prevent update action from updating likes property of the blog

--- part5/server/controllers/blogs.js
+++ part5/server/controllers/blogs.js
@@ -89,6 +89,9 @@ blogsRouter.put('/:id', async (req, res) => {
   const user = await User.findById(decodedToken.id)
   const blog = await Blog.findById(req.params.id)
 
+  // likes should not be changed by put
+  payload.likes = blog.likes
+
   if (blog) {
     if (blog.user.toString() === user._id.toString()) {
       const { _id } = await Blog.findByIdAndUpdate(req.params.id, payload, {
@@ -104,6 +107,30 @@ blogsRouter.put('/:id', async (req, res) => {
     return res.status(401).send({ err: 'Unauthorized' })
   }
   return res.status(500).send({ err: 'Server Error' })
+})
+
+blogsRouter.patch('/:id', async (req, res) => {
+  const id = req.params.id
+  const body = { ...req.body }
+  const blog = await Blog.findById(id)
+
+  if (blog) {
+    if (body.likes === 'like') {
+      blog.likes += 1
+    }
+    if (body.likes === 'unlike') {
+      blog.likes -= 1
+    }
+    await blog.save()
+
+    const updatedBlog = await Blog.findOne({ _id: blog._id }).populate('user', {
+      id: 1,
+      name: 1,
+      username: 1,
+    })
+    return res.json(updatedBlog)
+  }
+  return res.status(404).send({ err: 'Blog does not exist' })
 })
 
 module.exports = blogsRouter
