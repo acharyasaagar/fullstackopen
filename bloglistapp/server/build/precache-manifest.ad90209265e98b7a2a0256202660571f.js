f290ffbf3e346998dfce8120b05ce989289d6aa7
acharyasagar <acharyasaagar@gmail.com>
2020-08-30 21:26:21 +0300

    online bloglist app

---
+++ bloglistapp/server/build/precache-manifest.ad90209265e98b7a2a0256202660571f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102559b115d656f467c1b2a05da6ce75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c4e92a9af6c3ed72e03",
+    "url": "/static/css/main.d2c7c17f.chunk.css"
+  },
+  {
+    "revision": "064cc767ef64a8455c27",
+    "url": "/static/js/2.497fd4cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.497fd4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4e92a9af6c3ed72e03",
+    "url": "/static/js/main.2d0fdd61.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
---
+++ bloglistapp/server/index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+const app = require('./app')
+const logger = require('./utils/logger')
+const { PORT } = require('./utils/constants')
+
+const server = http.createServer(app)
+server.listen(PORT, () => logger.info(`Server listening on port ${PORT}`))
